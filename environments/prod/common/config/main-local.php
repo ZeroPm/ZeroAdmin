3e630e8ddee849205a8f388437e4089ffb33a6ee
Zero <498159720@qq.com>
2019-04-24 11:51:48 +0800

    设置线上环境配置

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
             'username' => 'root',
-            'password' => '',
+            'password' => 'VMWkKCRIK0VY1LJ2',
             'charset' => 'utf8',
         ],
         'mailer' => [
